7d183468e841c791bdb22a71a87c1eaded45e137
Lorena Mondini <60662357+Lorslux@users.noreply.github.com>
2023-05-03 09:33:05 +0200

    Added routing and  a new service

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -1,11 +1,29 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input} from '@angular/core';
+import { Location } from '@angular/common';
 import { Hero } from '../hero';
+import { HeroService } from '../hero.service';
+import { ActivatedRoute } from '@angular/router';
 @Component({
   selector: 'app-hero-detail',
   templateUrl: './hero-detail.component.html',
-  styleUrls: ['./hero-detail.component.css']
+  styleUrls: ['./hero-detail.component.css'],
 })
 export class HeroDetailComponent {
   @Input() hero?: Hero;
 
+
+  constructor(private route: ActivatedRoute, private heroService: HeroService, private location: Location) {}
+  ngOnInit(): void {
+    this.getHero();
+  }
+
+  getHero(): void {
+    const id = Number(this.route.snapshot.paramMap.get('id'));
+    this.heroService.getHero(id)
+      .subscribe(hero => this.hero = hero);
+  }
+
+  goBack(): void {
+    this.location.back();
+  }
 }
